3c69f995c57eb8c9ff7cd5b2803b22dfa727b9d8
KirillMar <re4r@bk.ru>
2022-12-09 16:44:25 +0300

    VIII

---
+++ ImplementMergeSort.js
@@ -0,0 +1,37 @@
+function merger(arr1, arr2) {
+    let i = 0;
+    let j = 0;
+    const mergedArr = [];
+
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] > arr2[j]) {
+            mergedArr.push(arr2[j++]);
+        } else {
+            mergedArr.push(arr1[i++]);
+        }
+    }
+
+    while (i < arr1.length) {
+        mergedArr.push(arr1[i++]);
+    }
+
+    while (j < arr2.length) {
+        mergedArr.push(arr2[j++]);
+    }
+
+    return mergedArr;
+}
+
+function mergeSort(array) {
+    if (array.length === 1) {
+        return array;
+    }
+
+    const middle = Math.floor(array.length / 2);
+    const left = mergeSort(array.slice(0, middle));
+    const right = mergeSort(array.slice(middle));
+
+    return merger(left, right);
+}
+
+console.log(mergeSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92]));
